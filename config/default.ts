2050027c1dedbbfe852921720d278a0c084a6701
kchn9 <mat.kuchnia@gmail.com>
2022-12-09 21:53:44 +0100

    Create user model with password hashing

--- config/default.ts
+++ config/default.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 
 export default {
-  server: {
-    port: 3000,
-  },
-  db: {
-    username: process.env.MONGO_USERNAME,
-    password: process.env.MONGO_PASSWORD,
-    dbUri: process.env.MONGO_URI,
-  },
+    server: {
+        port: 3000,
+    },
+    db: {
+        username: process.env.MONGO_USERNAME,
+        password: process.env.MONGO_PASSWORD,
+        dbUri: process.env.MONGO_URI,
+    },
+    bcrypt: {
+        saltRounds: 10,
+    },
 };
---
+++ src/models/user.model.ts
@@ -0,0 +1,83 @@
+import mongoose, { Schema, model, Model, HydratedDocument } from "mongoose";
+import config from "config";
+import bcrypt from "bcrypt";
+import logger from "@/utils/logger";
+
+export interface IUser {
+    username: string;
+    email: string;
+    password: string;
+    createdAt: number;
+    updatedAt: number;
+}
+
+export interface IUserMethods {
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+export type UserModelType = Model<IUser, unknown, IUserMethods>;
+
+const userSchema: Schema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+// hash user password before save
+userSchema.pre(
+    "save",
+    async function (
+        next: mongoose.CallbackWithoutResultAndOptionalError
+    ): Promise<void> {
+        const user = this as HydratedDocument<IUser>;
+        if (!user.isModified("password")) {
+            return next();
+        }
+        const { saltRounds } = config.get<{ saltRounds: number }>("bcrypt");
+        const salt = await bcrypt.genSalt(saltRounds);
+        const oldPassword: string = user.password;
+        user.password = await bcrypt.hash(oldPassword, salt);
+        return next();
+    }
+);
+
+// login method
+userSchema.method(
+    "comparePassword",
+    async function (candidatePassword: string): Promise<boolean> {
+        try {
+            const user = this as HydratedDocument<IUser>;
+            const isPasswordCorrect = await bcrypt.compare(
+                candidatePassword,
+                user.password
+            );
+            return isPasswordCorrect;
+        } catch (e) {
+            logger.error(e);
+            return false;
+        }
+    }
+);
+
+userSchema.set("toJSON", {
+    transform(_doc, ret) {
+        delete ret["password"];
+        return ret;
+    },
+});
+
+export default model<IUser>("user", userSchema);
