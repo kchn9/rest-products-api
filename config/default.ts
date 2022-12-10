9b9f01318f7a50438d4cbbd6447c9be3b1ac4b6a
kchn9 <mat.kuchnia@gmail.com>
2022-12-10 13:21:52 +0100

    Add jwt config

--- config/default.ts
+++ config/default.ts
@@ -12,4 +12,11 @@ export default {
     bcrypt: {
         saltRounds: 10,
     },
+    jwt: {
+        privateKey: process.env.RSA_PRIVATE_KEY,
+        publicKey:
+            "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjbd3QYbeQZyeDnJBrCfjEEu3k\nv28W1RMS2EED4wCFfC8auUvbJHO1BtE3ckib3jwAf7rmf6uv68I/5G87KsQJRYaP\nfGSd/0Rt2/69dwZrUEBf5FxuOnv4Xb9McuDIPWWP8cRlW3CMX4gFxsAT0heirTtu\noqra/mR3h7PWb33zIQIDAQAB\n-----END PUBLIC KEY-----",
+        tokenTtl: "15m",
+        refreshTokenTtl: "1y",
+    },
 };
