0d4f9f9204ee589412cbb29ad85b9e478a21a867
kchn9 <mat.kuchnia@gmail.com>
2022-12-13 19:06:02 +0100

    feat: Split config into 2 dep on env

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ import UserController from "./controllers/user.controller";
 import SessionController from "./controllers/session.controller";
 import ProductController from "./controllers/product.controller";
 
-// todo validate .env
-
 const { port } = config.get<{ port: number }>("server");
 const app = new App(
     [new UserController(), new SessionController(), new ProductController()],
